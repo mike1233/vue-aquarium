8d7de13598619d6b808529e0b538ff380f572ea3
Mike Schepers <mike.schepers@infocaster.net>
2024-02-04 20:43:40 +0100

    moved gamestate to app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import Aquarium from './Aquarium.vue';
-import FishForm from './FishForm.vue';
-import type { Fish } from './models/fish.model';
+import { computed, onMounted, reactive } from "vue";
+import Aquarium from "./Aquarium.vue";
+import FishForm from "./FishForm.vue";
+import type { Fish } from "./models/fish.model";
 
-const fish = ref<Fish[]>([]);
+const state = reactive<{ fish: Fish[]; isPaused: boolean }>({
+  fish: [],
+  isPaused: false,
+});
 
 const addFish = (newFish: Fish) => {
-    fish.value.push(newFish);
+  state.fish.push(newFish);
 };
 
+const togglePause = () => (state.isPaused = !state.isPaused);
+
+const retrieveGame = () => {
+  const savedFish = localStorage.getItem("fish");
+  if (savedFish) {
+    state.isPaused = true;
+    state.fish = JSON.parse(savedFish);
+  }
+};
+
+const saveGame = () => {
+  localStorage.setItem("fish", JSON.stringify(state.fish));
+};
+
+const pauseButtonText = computed(() => (state.isPaused ? "Resume" : "Pause"));
+
+onMounted(() => {
+  retrieveGame();
+});
 </script>
 <template>
-    <main class="flex gap-2 overflow-hidden">
-        <FishForm @add-fish="addFish" />
-        <Aquarium :fish="fish"/>
-    </main>
+  <main class="flex gap-2 overflow-hidden">
+    <FishForm @add-fish="addFish" />
+    <Aquarium v-bind="state">
+      <div class="game-buttons mt-2 mr-2 flex justify-end gap-2">
+        <button
+          class="bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded-md transition-colors"
+          @click="togglePause"
+        >
+          {{ pauseButtonText }}
+        </button>
+        <button
+          class="bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded-md transition-colors"
+          @click="saveGame"
+        >
+          Save
+        </button>
+      </div>
+    </Aquarium>
+  </main>
 </template>
-
--- src/Aquarium.vue
+++ src/Aquarium.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import { computed, onMounted, reactive, ref, watch } from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
 import FishComponent from "./Fish.vue";
 import type { Fish, FishExtended } from "./models/fish.model";
 import { getRandomInt } from "./utils";
 
 const props = defineProps<{
   fish: Fish[];
+  isPaused: boolean;
 }>();
 
 const aquarium = ref<HTMLDivElement>();
 const aquariumBounds = ref<DOMRect | null>(null);
-const isPaused = ref(false);
 
 const aquariumState = reactive<{ fish: FishExtended[] }>({
   fish: [],
@@ -18,25 +18,8 @@ const aquariumState = reactive<{ fish: FishExtended[] }>({
 
 const getAquariumBounds = () => aquarium.value?.getBoundingClientRect() ?? null;
 
-const togglePause = () => (isPaused.value = !isPaused.value);
-
-const retrieveGame = () => {
-  const savedFish = localStorage.getItem("fish");
-  if (savedFish) {
-    isPaused.value = true;
-    aquariumState.fish = JSON.parse(savedFish);
-  }
-};
-
-const saveGame = () => {
-  localStorage.setItem("fish", JSON.stringify(aquariumState.fish));
-};
-
-const pauseButtonText = computed(() => (isPaused.value ? "Resume" : "Pause"));
-
 onMounted(() => {
   aquariumBounds.value = getAquariumBounds();
-  retrieveGame();
 
   window.addEventListener(
     "resize",
@@ -75,20 +58,8 @@ onMounted(() => {
     ref="aquarium"
     class="relative bg-aquarium bg-center bg-cover bg-no-repeat w-[75vw] h-screen"
   >
-    <div class="game-buttons mt-2 mr-2 flex justify-end gap-2">
-      <button
-        class="bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded-md transition-colors"
-        @click="togglePause"
-      >
-        {{ pauseButtonText }}
-      </button>
-      <button
-        class="bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded-md transition-colors"
-        @click="saveGame"
-      >
-        Save
-      </button>
-    </div>
+    <slot></slot>
+
     <template v-if="aquariumBounds">
       <FishComponent
         v-for="(fish, index) in aquariumState.fish"
